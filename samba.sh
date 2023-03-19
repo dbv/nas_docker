6d0f0a2e349995b9239454cc7d61a88721bf2a72
liuhui <luvroot@126.com>
2023-03-19 15:31:35 +0800

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -7,4 +7,4 @@ docker_data=/home/nas/disk/1t
 docker stop $docker_name
 docker rm $docker_name
 
-docker run -it --name $docker_name --restart=always -m 512m  -p 139:139 -p 445:445 -v $docker_data:/mount -d dperson/samba -u "nas;nas" -e SMB_EXTRA_OPTS="-DSENDFILE64"  -s "nas;/mount/;yes;no;no;all;none"
+docker run -it --name $docker_name --restart=always -e SMB_EXTRA_OPTS="-DSENDFILE64" -m 512m  -p 139:139 -p 445:445 -v $docker_data:/mount -d dperson/samba -u "nas;nas" -s "nas;/mount/;yes;no;no;all;none"
