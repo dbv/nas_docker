03e71d0f5babe8f629269d8c5b5ca689fbeede2a
liuhui <luvroot@126.com>
2023-07-27 10:57:44 +0800

    Update alidrive.sh

--- alidrive.sh
+++ alidrive.sh
@@ -16,3 +16,5 @@ docker run -d --name=aliyundrive-webdav --restart=unless-stopped -p 8888:8080 \
   -e WEBDAV_AUTH_USER=root \
   -e WEBDAV_AUTH_PASSWORD=123 \
   messense/aliyundrive-webdav
+
+# 其中，REFRESH_TOKEN 环境变量为你的阿里云盘 refresh_token，WEBDAV_AUTH_USER 和 WEBDAV_AUTH_PASSWORD 为连接 WebDAV 服务的用户名和密码。
